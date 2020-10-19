8eed430e852117e7527ad7d15a62f30378b07c90
kristall-93 <kristushapaf@yandex.ru>
2020-10-19 18:35:59 +0300

    added adaptive to image gallery section

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,95 @@
 $(document).ready(function () {
 
     // Открытие search__explore-dropdown :
+
     $('.search__explore').on('click', function () {
         if ($('.search__explore-dropdown').hasClass('active')) {
-            $(".search__explore-dropdown").removeClass('active').fadeOut();
+            $('.search__explore-dropdown').removeClass('active').fadeOut();
+            $('.search__explore-dropdown-bg').removeClass('active').fadeOut();
+            $('.wrapper').css('position', 'static');
+            // $(".wrapper").removeClass('hidden');   
         }
         else {
-            $(".search__explore-dropdown").fadeIn().addClass('active');
+            $('.search__explore-dropdown').fadeIn().addClass('active');
+            $('.search__explore-dropdown-bg').fadeIn().addClass('active');
+            $('.wrapper').css('position', 'fixed');
+            // $(".wrapper").addClass('hidden');
+
         }
     });
-    // Закрытие search__explore-dropdown при клике в стороне :    
+    // Закрытие search__explore-dropdown при клике в стороне :        
     $(document).mouseup(function (e) {
-        var container__dropdown = $(".search__explore-dropdown");
+        var container__dropdown = $('.search__explore-dropdown');
         var container__search__explore = $(".search__explore");
         if (!container__dropdown.is(e.target) && container__dropdown.has(e.target).length === 0 && !container__search__explore.is(e.target) && container__search__explore.has(e.target).length === 0) {
             if ($('.search__explore-dropdown').hasClass('active')) {
                 $('.search__explore-dropdown').removeClass('active').fadeOut();
+                $('.search__explore-dropdown-bg').removeClass('active').fadeOut();
+                $('.wrapper').css('position', 'static');
+                // $(".wrapper").removeClass('hidden');   
             }
         }
     });
+    // Закрытие search__explore-dropdown при нажатии на кнопку :   
+    $('.search__explore-close').on('click', function () {
+        $('.search__explore-dropdown').removeClass('active').fadeOut();
+        $('.search__explore-dropdown-bg').removeClass('active').fadeOut();
+        $('.wrapper').css('position', 'static');
+        // $(".wrapper").removeClass('hidden');
+    });
+
+
+    // Открытие user-menu__dropdown :
+
+    $('.menu__btn, .user-menu__box').on('click', function () {
+        if ($('.user-menu__dropdown').hasClass('active')) {
+            $(".user-menu__dropdown").removeClass('active').fadeOut();
+            $('body, html').css('overflow-y', 'auto');
+        }
+        else {
+            $(".user-menu__dropdown").fadeIn().addClass('active');
+            $('body, html').css('overflow', 'hidden');
+
+        }
+    });
+    // Закрытие user-menu__dropdown при клике в стороне :    
+     $(document).mouseup(function (e) {
+        var container__user__menu = $('.user-menu');
+        if (!container__user__menu.is(e.target) && container__user__menu.has(e.target).length === 0) {
+            if ($('.user-menu__dropdown').hasClass('active')) {
+                $('.user-menu__dropdown').removeClass('active').fadeOut();
+                $('body, html').css('overflow-y', 'auto');
+            }
+        }
+    });
+    // $(document).mouseup(function (e) {
+    //     var container__dropdown = $('.user-menu__dropdown');
+    //     var container__menu__btn = $('.menu__btn');
+    //     var container__user__menu__box = $('.user-menu__box');
+    //     if (!container__dropdown.is(e.target) && container__dropdown.has(e.target).length === 0 && !container__menu__btn.is(e.target) && container__menu__btn.has(e.target).length === 0 && !container__user__menu__box.is(e.target) && container__user__menu__box.has(e.target).length === 0) {
+    //         if ($('.user-menu__dropdown').hasClass('active')) {
+    //             $('.user-menu__dropdown').removeClass('active').fadeOut();
+    //             $('body, html').css('overflow-y', 'auto');
+    //         }
+    //     }
+    // });
+   
+    
+    // Переключение светлой и темной темы:
+
+    $('.btn-dark-mode').on('click', function () {
+        $('body').addClass('dark');
+        $('.btn-light-mode').removeClass('active'); 
+        $('.btn-dark-mode').addClass('active');        
+    });
+    $('.btn-light-mode').on('click', function () {
+        $('body').removeClass('dark');
+        $('.btn-dark-mode').removeClass('active');        
+        $('.btn-light-mode').addClass('active'); 
+    });
 
     // переключение вкладок recommended :
+
     $('.recommended__inner .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
         $('.recommended__inner').find('.tab-item').removeClass('active-tab').hide();
@@ -31,6 +100,7 @@ $(document).ready(function () {
     });
 
     // Слайдер секции IMAGE GALLERY : 
+
     $('.gallery__slider').slick({
         dots: true,
         slidesToShow: 1,
@@ -38,17 +108,10 @@ $(document).ready(function () {
         infinite: false,
         prevArrow: '<button class="slider__arrow-prev"><img src="images/slider-arrow-prev.svg" alt=""></button>',
         nextArrow: '<button class="slider__arrow-next"><img src="images/slider-arrow-next.svg" alt=""></button>',
-    });
-
-    // переключение слайдов нажатием на слайд :
-
-    // $('.gallery__slider').click(function () {
-    //     $("#gallery__slider").slick('slickNext');
-    // });
-
+    });    
 
     // Слайдеры в popup-gallery :
-
+    
     $('.popup__slider-main').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -72,12 +135,12 @@ $(document).ready(function () {
         nextArrow: '<button class="popup__slider__arrow-next"><img src="images/gallery-arrow.svg" alt=""></button>',
     });
 
-    $('.slider-item__open-gallery').on('click', function () {    
-        $('.popup-gallery').addClass('active');        
+    $('.slider-item__open-gallery').on('click', function () {
+        $('.popup-gallery').addClass('active');
     });
 
-    $('.gallery__preview').on('click', function () {    
-        $('.popup-gallery').addClass('active');        
+    $('.gallery__preview').on('click', function () {
+        $('.popup-gallery').addClass('active');
     });
 
 
